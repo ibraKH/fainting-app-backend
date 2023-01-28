1d1e4fe27d9f635ac3e6e2b3f10a276ec9a6b728
ibraKH <ibrakh80@gmail.com>
2023-01-28 23:20:41 +0300

    v1.0.0

--- database/connection.js
+++ database/connection.js
@@ -1,16 +1,15 @@
-const dotenv = require("dotenv");
-const { Pool } = require("pg");
+const dotenv = require('dotenv');
 
 dotenv.config();
 
+const { Pool } = require("pg");
+
+const isProduction = process.env.NODE_ENV === "production";
 
 
 const pool = new Pool({
-    user: 'postgres',
-    database: process.env.DATABASE, 
-    password: process.env.PASSWORD,
-    port: 5432,
-    host: "localhost",
+    connectionString: process.env.DATABASE_URL || `postgresql://postgres:${process.env.PG_PASSWORD}@localhost:5432/${process.env.PG_DATABASE}`,
+    ssl: process.env.DATABASE_URL ? true : false
 })
 
 module.exports = pool;
